7c31de641af2776319aaa0a410dc78176ef6c412
Carl Vander <carl.vander@matsmart.se>
2022-12-02 13:22:46 +0100

    Day 2 in the books

--- solutions/day2.py
+++ solutions/day2.py
@@ -1,2 +1,39 @@
+MOVES = {"A": 1, "B": 2, "C": 3, "X": 1, "Y": 2, "Z": 3}
+BEAT_BY = {1: 2, 2: 3, 3: 1}
+BEATS = {1: 3, 2: 1, 3: 2}
+
+
+def _score(them, you) -> int:
+    if them == you:
+        return 3 + you
+    elif BEAT_BY[them] == you:
+        return 6 + you
+    else:
+        return you
+
+
 def day2_a(data):
-    return 0
+    score = 0
+    for row in data:
+        them, you = [MOVES[move] for move in row.split(" ")]
+        score += _score(them, you)
+
+    return score
+
+
+def day2_b(data):
+    score = 0
+    for row in data:
+        them, result = row.split(" ")
+        them = MOVES[them]
+
+        if result == "X":
+            you = BEATS[them]
+        elif result == "Y":
+            you = them
+        else:
+            you = BEAT_BY[them]
+
+        score += _score(them, you)
+
+    return score
--- tests/test_day2.py
+++ tests/test_day2.py
@@ -1,9 +1,26 @@
-from solutions.day2 import day2_a
+from solutions.day2 import day2_a, day2_b
 import pytest
 
 
-@pytest.mark.skip
 @pytest.mark.parametrize("filename", ("day2_sample.txt",))
-def test_day2_a(aoc_input):
+def test_day2_a_sample(aoc_input):
     actual = day2_a(data=aoc_input)
     assert actual == 15
+
+
+@pytest.mark.parametrize("filename", ("day2.txt",))
+def test_day2_a(aoc_input):
+    actual = day2_a(data=aoc_input)
+    assert actual == 13009
+
+
+@pytest.mark.parametrize("filename", ("day2_sample.txt",))
+def test_day2_b_sample(aoc_input):
+    actual = day2_b(data=aoc_input)
+    assert actual == 12
+
+
+@pytest.mark.parametrize("filename", ("day2.txt",))
+def test_day2_b(aoc_input):
+    actual = day2_b(data=aoc_input)
+    assert actual == 10398
